d0d39463a4d81af13f3756464e133b9fef3459b0
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-08-11 14:15:32 +0530

    Initial Commit

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-full h-full">
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body, html, #__nuxt, #__layout {
+  @apply w-full h-full
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="h-full w-full flex justify-center items-center">
+    <h1 class="text-6xl font-bold tracking-widest">
+      <span>SIN<span style="color: crimson">I</span>X</span>
+    </h1>
+  </div>
+</template>
