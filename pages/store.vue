1c78c3dd61d4fd5983e3304ab4f318c50fdb9735
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-09-14 08:29:32 +0530

    Add store coming soon

---
+++ pages/store.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex justify-center items-center flex-col h-full">
+    <h1 class="text-6xl font-bold">
+      S<span class="crimson">I</span>NIX
+      ST<span class="crimson">O</span>RE
+    </h1>
+    <h2 class="text-4xl animate-bounce">
+      COM<span class="crimson">I</span>NG SOON
+    </h2>
+  </div>
+</template>
+
+<style scoped>
+.crimson {
+  color: crimson;
+}
+
+</style>
