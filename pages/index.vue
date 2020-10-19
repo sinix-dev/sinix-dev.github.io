5cb4c370678368d4442190a60fd40846e07f421f
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-10-19 11:24:55 +0530

    refactor: open form for beta registration

--- pages/index.vue
+++ pages/index.vue
@@ -3,51 +3,14 @@
     <h1 class="text-6xl font-bold tracking-widest py-4">
       <img src="../static/images/logo-light.png" width="200px">
     </h1>
-    <h5 class="text-2xl tracking-wide">
+    <h5 class="text-2xl tracking-wide text-center">
       TWO FEET LONG GAMING PLATFORM
     </h5>
     <div style="height: 20px" />
     <div>
-      <input
-        v-model="email"
-        class="p-2 border email"
-        placeholder="Email Address"
-      >
-      <span @click="signUp">
+      <a href="https://forms.gle/uPF6kFzHcEnZB16r7" target="_blank">
         <ForBeta />
-      </span>
+      </a>
     </div>
   </div>
 </template>
-
-<script>
-import axios from 'axios'
-
-export default {
-  data () {
-    return {
-      email: ''
-    }
-  },
-  methods: {
-    signUp () {
-      const url = `https://ci6g2hzboc.execute-api.ap-south-1.amazonaws.com/v1/beta/${this.email}`
-
-      axios
-        .post(url)
-        .then((response) => {
-          console.log(response)
-        })
-        .catch((err) => {
-          console.log(err)
-        })
-    }
-  }
-}
-</script>
-
-<style scoped>
-.email {
-  width: 350px;
-}
-</style>
