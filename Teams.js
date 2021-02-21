c2c50df050494d82b53987630f4b8c99a8b79654
NikitaTulsani <68957298+NikitaTulsani@users.noreply.github.com>
2021-02-21 17:46:29 +0530

    Add files via upload
    Added teams pages and other assests

---
+++ Teams.js
@@ -0,0 +1,117 @@
+const persons = [
+  {
+    name: "Ritvij",
+    photo: "../assests/images/Ritvij.jpg",
+    title: "Mobile Ninja",
+    bio:
+      "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+    social: {
+      facebook: "#",
+      twitter: "https://twitter.com/ritvij14_?s=08",
+      linkedin: "https://www.linkedin.com/in/rks-1410",
+    },
+  },
+  {
+    name: "Harsh",
+    photo: "../assests/images/Harsh.jpg",
+    title: "Mobile Master",
+    bio:
+      "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+    social: {
+      facebook: "#",
+      twitter: "https://twitter.com/knyttneve",
+      linkedin: "#",
+    },
+  },
+  {
+    name: "Nikita Tulsani",
+    photo: "../assests/images/Nikita.jpeg",
+    title: "Web dev",
+    bio:
+      "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+    social: {
+      facebook: "#",
+      twitter: "https://twitter.com/Nikita71619428",
+      linkedin: "https://www.linkedin.com/in/nikita-tulsani-128a551b0/",
+    },
+  },
+  {
+    name: "Rukaiya Memon",
+    photo: "../assests/images/Rukaiya.jpeg",
+    title: "Web dev",
+    bio:
+      "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+    social: {
+      facebook: "#",
+      twitter: "https://twitter.com/knyttneve",
+      linkedin: "#",
+    },
+  },
+];
+
+const app = new Vue({
+  el: "#app",
+  data() {
+    return {
+      persons: persons,
+      selectedPersonIndex: null,
+      isSelected: false,
+      selectedPerson: null,
+      inlineStyles: null,
+      isReady: false,
+      isOk: false,
+      selectedPersonData: {
+        name: null,
+        title: null,
+        photo: null,
+        social: {
+          facebook: null,
+          twitter: null,
+          linkedin: null,
+        },
+      },
+    };
+  },
+  methods: {
+    selectPerson(index, el) {
+      if (!this.isOk) {
+        this.selectedPersonIndex = index;
+        this.isSelected = true;
+        el.target.parentElement.className == "person-details"
+          ? (this.selectedPerson = el.target.parentElement.parentElement)
+          : (this.selectedPerson = el.target.parentElement);
+
+        this.selectedPerson.classList.add("person-selected");
+        this.selectedPerson.setAttribute(
+          "style",
+          `width:${this.selectedPerson.offsetWidth}px;`
+        );
+        this.selectedPersonData = this.persons[index];
+        window.setTimeout(() => {
+          this.inlineStyles = `width:${this.selectedPerson.offsetWidth}px;height:${this.selectedPerson.offsetHeight}px;left:${this.selectedPerson.offsetLeft}px;top:${this.selectedPerson.offsetTop}px;position:fixed`;
+          this.selectedPerson.setAttribute("style", this.inlineStyles);
+        }, 400);
+        window.setTimeout(() => {
+          this.isReady = true;
+          this.isOk = true;
+        }, 420);
+      } else {
+        this.reset();
+      }
+    },
+    reset() {
+      this.isReady = false;
+      window.setTimeout(() => {
+        this.selectedPerson.classList.add("person-back");
+      }, 280);
+      window.setTimeout(() => {
+        this.selectedPerson.setAttribute("style", "");
+      }, 340);
+      window.setTimeout(() => {
+        this.isSelected = false;
+        this.selectedPerson.classList.remove("person-back", "person-selected");
+        this.isOk = false;
+      }, 400);
+    },
+  },
+});
